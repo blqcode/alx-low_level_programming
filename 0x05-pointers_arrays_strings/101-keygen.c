77cab8e7de57519448de5f6a77aefbd6d7ee662d
blqcode <ertalb364@gmail.com>
2023-09-18 12:10:46 -0700

    new

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,39 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
+#include <ctype.h>
+#include <string.h>
+
+#define PASSWORD_LENGTH 100
+#define CHECKSUM 2772
+
+
 
 /**
- * main - print password.
- *
- * Return: 0.
- */
+* generatePassword - A function that generate a random password an place it
+* inside an allucated variable named password
+* @_sum: A pointer to a variable that keep tracks genretad password
+* ascii value sum.
+* Return: The allocated char* password
+*/
+char *generatePassword(int *_sum) 
+{
+	const char alphanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *password = malloc(PASSWORD_LENGTH + 1);
+	int i;
 
+	srand(time(NULL));
+	for (i = 0; *_sum < CHECKSUM - (127); ++i) {
+		password[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+		*_sum += (int) password[i];
+	}
+	password[i] = '\0';
+	return password;
+}
+
+/**
+ * main - Entry point
+ * Description: A program that generetes a valid key for the program 101-crackme
+ * Return: 0
+ */
 int main(void)
 {
-	int ascii = 2772, i = 0, j, random;
-	char password[100];
-	time_t t;
+	int sum = 0;
+	char *password = generatePassword(&sum);
 
-	srand((int) time(&t));
-	while (ascii > 126)
-	{
-		random = rand() % 126;
-		password[i] = random;
-		ascii -= random;
-		i++;
-	}
-	if (ascii > 0)
-		password[i] = ascii;
-	else
-	{
-		i--;
-	}
-	
+	char *difference = malloc(2);
+	difference[0] = CHECKSUM - sum;
+	difference[1] = '\0';
+	strcat(password, difference);
 
-	for (j = 0; j <= i; j++)
-	{
-		printf("%c", password[j]);
-	}
+	printf("%s", password);
+
+	free(password);
+	free(difference);
 	return (0);
 }
