ae54f34aa7a5f8a36b3252f9b37b0fa304fa62ad
blqcode <ertalb364@gmail.com>
2023-10-05 20:38:48 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,131 +1,102 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
 
 /**
- * multiply - Multiplies two positive numbers
- * @num1: The first number to multiply
- * @num2: The second number to multiply
- */
-void multiply(char *num1, char *num2)
-{
-	int len1 = strlen(num1);
-	int len2 = strlen(num2);
-	int total_length = len1 + len2;
-	char *result = allocate_memory(total_length + 1);
-	int carry = 0;
-
-	reverse_string(num1);
-	reverse_string(num2);
-
-	for (int i = 0; i < len1; i++)
-	{
-		carry = 0;
-
-		for (int j = 0; j < len2; j++)
-		{
-			int digit1 = num1[i] - '0';
-			int digit2 = num2[j] - '0';
-
-			int temp = (result[i + j] - '0') + (digit1 * digit2) + carry;
-			result[i + j] = (temp % 10) + '0';
-			carry = temp / 10;
-		}
-
-		result[i + len2] += carry;
-	}
-
-	reverse_string(result);
-	printf("%s\n", result);
-
-	free(result);
-}
-
-/**
- * is_positive_number - Checks if a string represents a positive number
- * @num: The string to check
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: 1 if the string represents a positive number, 0 otherwise
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int is_positive_number(const char *num)
+int is_digit(char *s)
 {
-	for (int i = 0; num[i] != '\0'; i++)
-	{
-		if (num[i] < '0' || num[i] > '9')
-			return (0);
-	}
+	int i = 0;
 
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
 	return (1);
 }
 
 /**
- * print_error_and_exit - Prints an error message and exits with status 98
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
-void print_error_and_exit(void)
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
 {
 	printf("Error\n");
 	exit(98);
 }
 
 /**
- * reverse_string - Reverses a string in place
- * @str: The string to reverse
- */
-void reverse_string(char *str)
-{
-	int i = 0;
-	int j = strlen(str) - 1;
-
-	while (i < j)
-	{
-		char temp = str[i];
-		str[i] = str[j];
-		str[j] = temp;
-		i++;
-		j--;
-	}
-}
-
-/**
- * add_strings - Adds a number string to the result string
- * @result: The result string
- * @num: The number string to add
- */
-void add_strings(char *result, char *num)
-{
-	int carry = 0;
-	int len = strlen(num);
-
-	for (int i = 0; i < len; i++)
-	{
-		int digit = result[i] - '0' + num[i] - '0' + carry;
-		result[i] = (digit % 10) + '0';
-		carry = digit / 10;
-	}
-
-	while (carry)
-	{
-		result[len] = (carry % 10) + '0';
-		carry /= 10;
-		len++;
-	}
-
-	result[len] = '\0';
-}
-
-/**
- * allocate_memory - Allocates memory dynamically
- * @size: The size of the memory to allocate
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Pointer to the allocated memory
+ * Return: always 0 (Success)
  */
-char *allocate_memory(int size)
+int main(int argc, char *argv[])
 {
-	char *ptr = malloc(size * sizeof(char));
-	if (ptr == NULL)
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		print_error_and_exit();
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	return (ptr);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
+
