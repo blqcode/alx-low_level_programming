4a7fb5172d112601283b0c0c285f859af932133f
blqcode <ertalb364@gmail.com>
2023-10-09 22:49:12 +0200

    preprocessor

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef MY_MACROS_H
+#define MY_MACROS_H
+
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point to the program
+ * Description: Prints the name of the current input file
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
