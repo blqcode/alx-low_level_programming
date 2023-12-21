db709b153501739a222629c93022bebb1f6b04b4
blqcode <ertalb364@gmail.com>
2023-12-22 00:03:31 +0200

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,26 +1,30 @@
-#include <string.h>
-#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
- * *hash_table_create - creates a hash table.
- * @size: Size of the array
+ * hash_table_create - creates a hash table with a given size
  *
- * Return: Pointer to the newly created hash table.
+ * @size: size of the hash table
+ * Return: the created hash table, or NULL if function fails
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *table;
+	hash_node_t **array;
+	unsigned long int i;
 
-	if (size < 1)
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
+
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
 		return (NULL);
-	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_table_t));
-	if (new_table->array == NULL)
-		return (NULL);
-	memset(new_table->array, 0, sizeof(hash_table_t));
-	return (new_table);
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+
+	table->array = array;
+	table->size = size;
+
+	return (table);
 }
