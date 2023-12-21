9308dc8c8057770ec3354a30702234f822802693
blqcode <ertalb364@gmail.com>
2023-12-21 23:54:17 +0200

    0x01a

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
+ * *hash_table_create - creates a hash table.
+ * @size: Size of the array
  *
- * Return: pointer to the newly created hash table
+ * Return: Pointer to the newly created hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	unsigned long int i;
+	hash_table_t *new_table;
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	if (size < 1)
 		return (NULL);
-	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
-	if (hash_table->array == NULL)
-	{
-		free(hash_table);
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
 		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
-	return (hash_table);
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_table_t));
+	if (new_table->array == NULL)
+		return (NULL);
+	memset(new_table->array, 0, sizeof(hash_table_t));
+	return (new_table);
 }
